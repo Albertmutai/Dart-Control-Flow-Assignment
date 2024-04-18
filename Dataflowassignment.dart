11ff18829f32c3e362efb6a990880a04dd7f9fb6
Albertmutai <55190986+Albertmutai@users.noreply.github.com>
2024-04-18 17:23:26 +0300

    Add files via upload

---
+++ Dataflowassignment.dart
@@ -0,0 +1,15 @@
+
+void main()
+ {
+  var num = 10;
+  if (num> 10) {
+    print("Your number is greater than 10");
+  } else if (num<10) {
+    print("Your number is less than 10");
+  } else if(num==10) {
+    print("Your number is equal to 10");
+  }
+else { 
+  print(" Your number did meet our criteria");
+}
+}
